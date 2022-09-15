8b31ddb9c553311dc6f8c60bf8a8bca1e58dd137
theexcel <xclolatunbosun@gmail.com>
2022-09-15 21:27:42 +0100

    created variable for the first number, second number and last number

--- src/Calculator.java
+++ src/Calculator.java
@@ -14,6 +14,9 @@ public class Calculator implements ActionListener{
 
     Font myFont = new Font("Ink Free", Font.BOLD, 30);
 
+    double num1 = 0, num2 = 0, result = 0;
+
+
     Calculator(){
 
     }
