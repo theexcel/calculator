cea8849bbba3f534b648d45a67f23490c2913fa5
theexcel <xclolatunbosun@gmail.com>
2022-09-16 13:26:06 +0100

    iterating the function buttons

--- src/Calculator.java
+++ src/Calculator.java
@@ -47,6 +47,11 @@ public class Calculator implements ActionListener{
     functionButtons[6] = delButton;
     functionButtons[7] = clrButton;
 
+    for( int i = 0; i < 8; i++){
+        functionButtons[i].addActionListener(this);
+        functionButtons[i].setFont(myFont);
+        functionButtons[i].setFocusable(false);
+    }
 
     frame.add(textField);
     frame.setVisible(true);
